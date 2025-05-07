d38b268b1c08a4fdd2d1eb9e9c4e059d4616e1a7
Klamer <b.klamecki@wp.pl>
2025-05-07 13:41:03 +0200

    Haslo do sesji generowane jest losowo i jest mozliwosc regeneracji hasla istniejacej sesji.

--- fotoapp/admin.py
+++ fotoapp/admin.py
@@ -7,5 +7,13 @@ class PhotoInline(admin.TabularInline):
 
 class SessionAdmin(admin.ModelAdmin):
     inlines = [PhotoInline]
+    list_display = ('name', 'password', 'access_token', 'created_at')
+    actions = ['regenerate_password']
+
+    @admin.action(description='Wygeneruj nowe hasło')
+    def regenerate_password(self, request, queryset):
+        for session in queryset:
+            session.regenerate_password()
+        self.message_user(request, f'Wygenerowano nowe hasła dla {queryset.count()} sesji.')
 
 admin.site.register(Session, SessionAdmin)
--- fotoapp/models.py
+++ fotoapp/models.py
@@ -1,20 +1,32 @@
 import uuid
 from django.db import models
+from django.utils.crypto import get_random_string
 
 class Session(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    password = models.CharField(max_length=100)
+    password = models.CharField(max_length=100, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     access_token = models.CharField(max_length=36, unique=True, blank=True)
 
     def save(self, *args, **kwargs):
         if not self.access_token:
             self.access_token = str(uuid.uuid4())
+
+        if not self.password:
+            self.password = self.generate_new_password()
+
         super().save(*args, **kwargs)
+    
+    def generate_new_password(self):
+        return get_random_string(12)
     
     def generate_new_token(self):
         return str(uuid.uuid4())
+    
+    def regenerate_password(self):
+        self.password = self.generate_new_password()
+        self.save()
 
     def __str__(self):
         return self.name
